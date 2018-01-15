8edec1e07a3588161ffe3dfa02f304e7a536455c
Frederic Varni <frederic.varni@laerce.fr>
2018-01-15 21:34:21 +0100

    Ajout du DAO pour Niveau

---
+++ src/main/java/fr/laerce/gestionstages/dao/NiveauDAO.java
@@ -0,0 +1,39 @@
+package fr.laerce.gestionstages.dao;
+
+import fr.laerce.gestionstages.domain.Niveau;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+/**
+ * Projet pfmp-base
+ * Pour LAERCE SAS
+ * <p>
+ * Créé le  12/01/2018.
+ *
+ * @author fred
+ */
+
+@Repository
+public class NiveauDAO {
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public List<Niveau> findAll(){
+        return entityManager.createQuery("SELECT n FROM  Niveau n", Niveau.class).getResultList();
+    }
+
+    public void persist(Niveau niveau){
+        entityManager.merge(niveau);
+    }
+
+    public Niveau getById(Long id){
+        return entityManager.find(Niveau.class, id);
+    }
+
+    public void delete(Niveau niveau){
+        entityManager.remove(niveau);
+    }
+}
