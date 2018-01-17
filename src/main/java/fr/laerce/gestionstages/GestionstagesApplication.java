6afa2d99afcbef6f56337dd92b710ba53d4b6b34
Olivier Capuozzo <olivier.capuozzo@gmail.com>
2018-01-17 19:30:29 +0100

    Element de solution du premier exercice JPA et MVC le reste de la solution est principalement dans le starter guide 'Handling Form Submission' sur https://spring.io/guides. Points délicats : création d'un lien avec variable https://stackoverflow.com/questions/33753975/thymeleaf-using-path-variables-to-thhref et usage de Optional avec le repository

--- src/main/java/fr/laerce/gestionstages/GestionstagesApplication.java
+++ src/main/java/fr/laerce/gestionstages/GestionstagesApplication.java
@@ -1,69 +1,12 @@
 package fr.laerce.gestionstages;
 
-import fr.laerce.gestionstages.dao.DisciplineDAO;
-import fr.laerce.gestionstages.domain.Discipline;
-import fr.laerce.gestionstages.domain.Individu;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
-
 @SpringBootApplication
-public class GestionstagesApplication implements ApplicationRunner {
-    @PersistenceContext
-    EntityManager em;
-
-    @Autowired
-    DisciplineDAO ddao;
-
+public class GestionstagesApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(GestionstagesApplication.class, args);
-    }
-
-    @Override
-    @Transactional
-    public void run(ApplicationArguments args) throws Exception {
-
-        // passer spring.jpa.hibernate.ddl-auto=create
-        // exécuter une fois ce code
-        // passer spring.jpa.hibernate.ddl-auto=update
-        // et commenter le code à l'exception des 2 dernières instructions
-        // afin de vérifier que les Individus sont bien remontés via Discipline
-
-        Individu p = new Individu();
-        p.setNom("Martin");
-        p.setPrenom("Paul");
-        p.setCivilite("M.");
-
-        Discipline d = new Discipline();
-        d.setCode("Math");
-        d.setLibelle("Mathématique");
-
-        p.setDiscipline(d);
-        em.persist(d);
-        em.persist(p);
-
-        p = new Individu();
-        p.setNom("Martin");
-        p.setPrenom("Pauline");
-        p.setCivilite("Me");
-        p.setDiscipline(d);
-
-        em.persist(p);
-        System.out.println("professeurs de la discipline :"
-                + d.getProfesseurs());
-
-
-        Discipline d2 = ddao.getById(new Long(1));
-                //em.find(Discipline.class, new Long(1));
-        System.out.println("professeurs de la discipline :"
-                + d2.getProfesseurs());
-
     }
 }
--- src/main/java/fr/laerce/gestionstages/web/AdminController.java
+++ src/main/java/fr/laerce/gestionstages/web/AdminController.java
@@ -1,10 +1,21 @@
 package fr.laerce.gestionstages.web;
 
+import fr.laerce.gestionstages.dao.DisciplineRepository;
+import fr.laerce.gestionstages.domain.Discipline;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class AdminController {
+
+    @Autowired
+    DisciplineRepository repo;
 
     // après avoir étudier
     //  https://spring.io/guides/gs/accessing-data-jpa/
@@ -13,4 +24,23 @@ public class AdminController {
     // implémenter une interaction de création, suppression et liste
     // des objets de type Discipline
 
+    // voici un extrait de solution de suppression sans confirmation
+    // de l'utilisateur (TODO).
+
+    @GetMapping("/disciplines")
+    public String listeDisciplines(Model model) {
+        model.addAttribute("disciplines", repo.findAll());
+        return "disciplines";
+    }
+
+    @GetMapping("/deletedisciplines/{id}")
+    public String listeDisciplines(@PathVariable("id") long id) {
+        // le repository nous oblige à géger les pointeurs null
+        // via un Optional
+        Optional<Discipline> d = repo.findById(id);
+        if (d.isPresent()) {
+            repo.delete(d.get());
+        }
+        return "redirect:/disciplines";
+    }
 }
